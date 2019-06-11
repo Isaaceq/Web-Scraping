93234d0b2be2016a0d8b8e97c45b91c545c631a1
Isaac <isaac_25@aol.com>
2019-06-10 21:20:30 -0700

    updated npm packages and added npm Chalk

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const chalk = require('chalk')
 
-request('https://www.apple.com/', (error, response, html) => {
+const url = 'https://www.apple.com/';
+const log = console.log;
+
+
+request(url, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
 
     let getPhoneNum = $('.nowrap').text().split(' ');
   
-    getPhoneNum = getPhoneNum[3];
+    getPhoneNum = getPhoneNum[4];
     
     let phoneNum = getPhoneNum.substring(0, getPhoneNum.length - 1);
     
-    console.log(phoneNum);
+    log(chalk.yellow.bold.underline(phoneNum));
   }
 });
